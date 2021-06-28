3fde713d1bb12578fb5e471f8734a141264f84db
Afaque Wasim <76209126+afaquewasim@users.noreply.github.com>
2021-06-28 12:48:47 +0530

    Add files via upload

---
+++ bulb/code/bulb.js
@@ -0,0 +1,6 @@
+function setNewImg() {
+    document.getElementById("img").src = "../image/bulbon.jpg";
+}
+function setSameImg() {
+    document.getElementById("img").src = "../image/bulboff.png";
+}
